70a0f09d7fd975860d4328e4798f2255dbd92b0f
Mikkel Sørensen <vhk871@alumni.ku.dk>
2019-06-04 21:48:34 +0200

    Possible to add unknown users
    Signed-off-by: Mikkel Sørensen <vhk871@alumni.ku.dk>

--- add_users.py
+++ add_users.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 import numpy as np
 import sqlite3
-import sys
+import sys, os
 
 first_id = 1000
 
-if len(sys.argv) != 3:
-    print("Usage: add_users.py <name_list.csv> <database_name.db>")
-if len(sys.argv) == 3:
+if len(sys.argv) == 1:
+    print("Usage: ./add_users.py <name_list> [-a <int>] <database_name.db>")
+elif os.path.exists(sys.argv[len(sys.argv)-1]):
+    db_name = str(sys.argv[len(sys.argv)-1])
     students = np.genfromtxt(str(sys.argv[1]), delimiter=',', dtype=(str))
+    if "-a" in sys.argv:
+        amount = int(sys.argv[(int(sys.argv.index("-a") + 1))])
+        for i in range(0,amount):
+            students = np.append(students, [['Ukendt ' + str(i), '0', 'Deltager']], axis=0)
 
-    conn = sqlite3.connect(str(sys.argv[2]))
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
 
     id = first_id
     for s in students:
-        print(s)
         name, level, faction = s
         query = "(" + str(id) + ",\"" + name + "\",\"" + faction + "\"," + level + ")"
         c.execute("INSERT INTO users(id, name, faction, level) VALUES " + query)
         id += 1
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
