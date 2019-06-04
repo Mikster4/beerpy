7378156a093a1f25d02e29c8f47baeb5a7a65b4a
Mikkel Sørensen <vhk871@alumni.ku.dk>
2019-06-04 16:58:42 +0200

    Nicer argparsing
    Signed-off-by: Mikkel Sørensen <vhk871@alumni.ku.dk>

--- generate_barcode.py
+++ generate_barcode.py
@@ -7,6 +7,16 @@ import sys
 import os
 import shutil
 
+def get_accept():
+    char = input("\nAre these values correct? [y]/n: ")
+    if char == "y" or char == "Y":
+        return True
+    elif char == "n" or char == "N":
+        return False
+    else:
+        print("Sorry, I could not read the input. Please try again.")
+        get_accept()
+
 if len(sys.argv) == 1:
     print("Usage: ./generate_barcode.py [-C] <database_name.db>")
 elif os.path.exists(sys.argv[len(sys.argv)-1]):
@@ -17,9 +27,16 @@ elif os.path.exists(sys.argv[len(sys.argv)-1]):
     else:
         print("Barcodes directory already exists")
         print("Warning: You might want to run with '-C' to clear old barcodes")
-        if sys.argv[1] == "-C":
-            shutil.rmtree("barcodes/")
-            print("Removd all barcodes from directory")
+        if "-C" in sys.argv:
+            folder = 'barcodes'
+            for file in os.listdir(folder):
+                file_path = os.path.join(folder, file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print(e)
+            print("Removed all barcodes from directory")
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
 
