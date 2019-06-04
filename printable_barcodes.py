0f448e2911de34e8baa5a7596e622d365a7d6eba
Mikkel Sørensen <vhk871@alumni.ku.dk>
2019-06-04 21:36:07 +0200

    Finally working
    Signed-off-by: Mikkel Sørensen <vhk871@alumni.ku.dk>

--- printable_barcodes.py
+++ printable_barcodes.py
@@ -5,12 +5,13 @@ import numpy as np
 import sqlite3
 import os
 import glob, os, sys
+import shutil
 os.chdir("./")
 
 if len(sys.argv) == 1:
-    print("Usage: printable_barcodes.py <name_list.csv>")
+    print("Usage: printable_barcodes.py <database_name.db>")
 elif os.path.exists(sys.argv[len(sys.argv)-1]):
-    name_list = sys.argv[len(sys.argv)-1]
+    db_name = sys.argv[len(sys.argv)-1]
     if not os.path.exists('PDF'):
         os.mkdir('PDF')
         print("PDF directory created")
@@ -18,29 +19,46 @@ elif os.path.exists(sys.argv[len(sys.argv)-1]):
         print("PDF directory already exists")
         print("Warning: You might want to run with '-C' to clear old files")
         if sys.argv[1] == "-C":
-            shutil.rmtree("PDF/")
+            folder = 'PDF'
+            for file in os.listdir(folder):
+                file_path = os.path.join(folder, file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print(e)
             print("Removed all files from PDF directory")
 
-    names = open(name_list).readlines()
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute("SELECT users.name, users.faction FROM users WHERE 1 ORDER BY users.id ASC")
+    names = c.fetchall()
+    names = np.asarray(names)
+
     barcodes = []
     factions = []
     for n in names:
-        barcodes.append(n.split(',')[0].replace(' ', '_'))
-        factions.append(n.split(',')[2].replace(' ', '_'))
+        barcodes.append(n[0].replace(' ', '_'))
+        factions.append(n[1].replace(' ', '_'))
 
+    print("\nGenerating " + str(len(barcodes)) + " barcodes." )
+    print("\nPDFlatex information:")
     os.chdir("PDF/")
     workfile = "printable_barcodes.tex"
-    F = open(workfile,"a")
-    F.write("\\documentclass[a4paper,11pt]{article}")
-    F.write("\\usepackage{graphicx}")
-    F.write("\\usepackage[utf8]{inputenc}")
-    F.write("\\usepackage[T1]{fontenc}")
-    F.write("\\usepackage[danish]{babel}")
-    F.write("\\usepackage{array}")
-    F.write("\\usepackage{ragged2e}")
-    F.write("\\usepackage{subcaption}")
-    F.write("\\usepackage{float}")
-    F.write("\\begin{document}")
+    F = open(workfile,"w")
+    F.write("\\documentclass[a4paper,11pt]{article}\n")
+    F.write("\\usepackage{graphicx}\n")
+    F.write("\\usepackage[utf8]{inputenc}\n")
+    F.write("\\usepackage[T1]{fontenc}\n")
+    F.write("\\usepackage[danish]{babel}\n")
+    F.write("\\usepackage{array}\n")
+    F.write("\\usepackage{ragged2e}\n")
+    F.write("\\usepackage{subcaption}\n")
+    F.write("\\usepackage{float}\n")
+    F.write("\\usepackage[margin=0.5in]{geometry}\n")
+    F.write("\\begin{document}\n")
 
 
     c = 0
@@ -49,26 +67,25 @@ elif os.path.exists(sys.argv[len(sys.argv)-1]):
     F.write("\\begin{figure}[H]\n")
     row = ""
     while c < len(barcodes):
-        if c % 3 == 0 and not first:
-            F.write(row)
+        if c % 4 == 0 and not first:
             F.write("\\end{figure}\n")
             F.write("\\begin{figure}[H]\n")
             row = ""
-        row += "\\begin{subfigure}[t]{0.3\\textwidth}\n"
-        row += "\\centering\n"
-        row += "\\fbox{\\includegraphics[scale=0.6]{../barcodes/" + barcodes[c] + ".png}}\n"
-        row += "\\caption*{" + barcodes[c].replace('_', ' ') + " \\\ " + factions[c].replace('_',' ') + "}\n"
-        row += "\\end{subfigure}\n"
-
+        row += "\t\\begin{subfigure}[t]{0.24\\textwidth}\n"
+        row += "\t\t\\centering\n"
+        row += "\t\t\\fbox{\\includegraphics[scale=0.6]{../barcodes/" + barcodes[c] + ".png}}\n"
+        row += "\t\t\\caption*{" + barcodes[c].replace('_', ' ') + " \\\ " + factions[c].replace('_',' ') + "}\n"
+        row += "\t\\end{subfigure}\n"
+        F.write(row)
+        row = ""
         first = False
         c += 1
 
-    print(type(barcodes[0]))
     F.write("\\end{figure}\n")
     F.write("\\end{document}")
     F.close()
 
-    os.system("pdflatex printable_barcodes.tex")
-    #os.remove("output.tex")
+    os.system("pdflatex -interaction batchmode printable_barcodes.tex")
+    #os.remove("printable_barcodes.tex")
     os.remove("printable_barcodes.aux")
     os.remove("printable_barcodes.log")
